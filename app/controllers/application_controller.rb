8c0d6a12f8c4cc16b56e11f67ac1635db6d8f42e
takayuki <takayuki.suda.96@outlook.jp>
2016-11-20 16:02:33 +0900

    permit name key when registration#edit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,7 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
   # sign_upのときに、nameも許可する
     devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+    devise_parameter_sanitizer.permit(:account_update, keys:[:name])
   end
 
 end
