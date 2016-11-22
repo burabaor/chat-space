205ecd3d03feb93634ef4e844b57d8e10258d0eb
takayuki <takayuki.suda.96@outlook.jp>
2016-11-22 17:25:35 +0900

    use ffaker to email and password

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,12 @@
 require 'ffaker'
 
 FactoryGirl.define do
-
   factory :user do
+    var_password = FFaker::Internet.password
+    # define user
     name FFaker::Name.name
-    email 'test@tests'
-    password "122333"
-    password_confirmation '122333'
+    email FFaker::Internet.email
+    password var_password
+    password_confirmation var_password
   end
 end
