97f2840c222132ab5d2e4978427e198a37768833
takayuki <takayuki.suda.96@outlook.jp>
2016-11-24 10:21:59 +0900

    add group#new function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,14 @@
 class GroupsController < ApplicationController
   def new
-    
+    @group = Group.new
+  end
+  def create
+    Group.create(create_params)
+    redirect_to root and return
+  end
+
+  private
+  def create_params
+    params.require(:group).permit(:id, :name)
   end
 end
