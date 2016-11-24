8b4f6fded8ae4a35e2f389102fa445b2db908f15
takayuki <takayuki.suda.96@outlook.jp>
2016-11-24 11:04:04 +0900

    add edit function

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,9 +6,23 @@ class GroupsController < ApplicationController
     Group.create(create_params)
     redirect_to root and return
   end
+  def edit
+    @group = find_group
+  end
+  def update
+    @group = find_group
+    @group.update(update_params)
+    redirect_to root
+  end
 
   private
+  def find_group
+    Group.find(params[:id])
+  end
   def create_params
     params.require(:group).permit(:id, :name)
+  end
+  def update_params
+    params.require(:group).permit(:name)
   end
 end
