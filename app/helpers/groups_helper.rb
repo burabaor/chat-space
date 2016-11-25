2df5ffaac378899a68c393baccf594e59f75d4eb
takayuki <takayuki.suda.96@outlook.jp>
2016-11-25 10:20:23 +0900

    use ternary operator

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,10 +1,5 @@
 module GroupsHelper
   def user_is_member?(group_id, user_id)
-    member = GroupMember.find_by(group_id: group_id, user_id: user_id)
-    if member.present?
-      return true
-    else
-      return false
-    end
+    GroupMember.find_by(group_id: group_id, user_id: user_id).present? ? true : false
   end
 end
