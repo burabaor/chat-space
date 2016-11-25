fc87ddc5a5789b10d1fa85f738ba41cd1e83f49b
takayuki <takayuki.suda.96@outlook.jp>
2016-11-25 14:43:44 +0900

    sending a message function

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,16 @@
 class MessagesController < ApplicationController
   def index
     @group = Group.find(params[:group_id])
+    @message = Message.new
     @groups = Group.all
+  end
+  def create
+    Message.create(create_params)
+    redirect_to :back
+  end
+
+  private
+  def create_params
+    params.require(:message).permit(:body).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
