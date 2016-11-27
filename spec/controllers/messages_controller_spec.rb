678d5ca9d3c60527fe69a693ef8c7fc7920fefb0
takayuki <takayuki.suda.96@outlook.jp>
2016-11-27 21:23:43 +0900

    add context of not_sign_in_user

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -5,36 +5,52 @@ RSpec.describe MessagesController, :type => :controller do
   let(:user) { create(:user) }
   let(:message) { { message: attributes_for(:message), group_id: group.id } }
 
-  before do
-    sign_in user
+  context 'not sign_in user' do
+    describe 'GET #index' do
+      it "renders the :index template" do
+        get :index, group_id: group.id
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
+    describe 'POST #create' do
+      it "renders the :index template" do
+        post :create, message
+        expect(response).to redirect_to new_user_session_path
+      end
+    end
   end
 
-  # index
-  describe 'GET #index' do
+  context 'sign_in user' do
     before do
-      get :index, group_id: group.id
+      sign_in user
     end
-    it "renders the :index template" do
-      expect(response).to render_template :index
+    # index
+    describe 'GET #index' do
+      before do
+        get :index, group_id: group.id
+      end
+      it "renders the :index template" do
+        expect(response).to render_template :index
+      end
+      it "assigns the requested contact to @group" do
+        expect(assigns(:group)).to eq group
+      end
     end
-    it "assigns the requested contact to @group" do
-      expect(assigns(:group)).to eq group
-    end
-  end
 
-  # create
-  describe 'POST #create' do
-    it "renders the :index template" do
-      post :create, message
-      expect(response).to redirect_to group_messages_path(group)
-    end
-    it "has a text in flash-alert when message is empty" do
-      message[:message][:body] = ""
-      post :create, message
-      expect(flash[:alert]).to eq "メッセージを入力してください"
-    end
-    it "has one more message when message is saved" do
-      expect{ post :create, message }.to change{ Message.count }.by(1)
+    # create
+    describe 'POST #create' do
+      it "renders the :index template" do
+        post :create, message
+        expect(response).to redirect_to group_messages_path(group)
+      end
+      it "has a text in flash-alert when message is empty" do
+        message[:message][:body] = ""
+        post :create, message
+        expect(flash[:alert]).to eq "メッセージを入力してください"
+      end
+      it "has one more message when message is saved" do
+        expect{ post :create, message }.to change{ Message.count }.by(1)
+      end
     end
   end
 end
