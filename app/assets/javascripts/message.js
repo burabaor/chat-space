53e8c3a5e76545ea33c86261f4763d1c7c4fef6a
takayuki <takayuki.suda.96@outlook.jp>
2016-12-03 22:55:07 +0900

    change the part of reloaded message

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,7 @@
 $(function() {
-  // 自動更新のセッティング
-  if(location.href.match(/messages/)) {
-    setInterval("location.reload()",10000);
-  }
+  var url = location.href + ".json";
 
-  // 新規メッセージのhtmlテンプレート
+  // メッセージのhtmlテンプレート
   function buildHTML(message) {
     if (!message.image.url) message.image.url = "";
     var html =
@@ -25,12 +22,24 @@ $(function() {
     return html;
   }
 
+  // 自動更新のセッティング
+  if(location.href.match(/messages/))
+    setInterval(function() {
+      $.ajax({ url: url })
+      .done(function(data) {
+        $('.chat-body').empty();
+        $(data).each(function(i, message) {
+          var html = buildHTML(message);
+          $('.chat-body').append(html);
+        });
+      });
+    } ,10000);
+
   // メッセージ作成の非同期通信処理
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var $form = $('form#new_message').get(0);
     var fd = new FormData($form);
-    var url = location.href + '.json';
     $.ajax({
       type: 'POST',
       url: url,
