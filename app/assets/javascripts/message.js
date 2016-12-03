6bc877d736e129c35d37f340fc530fbe35328510
takayuki <takayuki.suda.96@outlook.jp>
2016-12-03 17:31:17 +0900

    add setinterval

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,10 @@
 $(function() {
+  // 自動更新のセッティング
+  if(location.href.match(/messages/)) {
+    setInterval("location.reload()",1000000);
+  }
+
+  // 新規メッセージのhtmlテンプレート
   function buildHTML(message) {
     if (!message.image.url) message.image.url = "";
     var html =
@@ -19,6 +25,7 @@ $(function() {
     return html;
   }
 
+  // メッセージ作成の非同期通信処理
   $('#new_message').on('submit', function(e) {
     e.preventDefault();
     var $form = $('form#new_message').get(0);
